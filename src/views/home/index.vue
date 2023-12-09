401252de24e1928cb82669a5f4b0c53064719e11
Sunalone <843258092@qq.com>
2023-12-09 18:55:12 +0800

    feat: 初始化template-vue-js

---
+++ src/views/home/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div>
+            <HelloWorld msg="Hello World" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+</script>
+<style scoped lang='less'></style>
